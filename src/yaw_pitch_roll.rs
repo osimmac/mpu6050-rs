669f444fabd6b50c467f0653c4cec9f610540e2b
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-03-02 15:25:16 +0100

    Change pitch calculation in Quaternion-to-YPR conversion

--- src/yaw_pitch_roll.rs
+++ src/yaw_pitch_roll.rs
@@ -1,8 +1,6 @@
 use crate::gravity::Gravity;
 use crate::quaternion::Quaternion;
 
-const PI: f64 = core::f64::consts::PI;
-
 #[derive(Debug, Copy, Clone)]
 pub struct YawPitchRoll {
     pub yaw: f32,
@@ -21,14 +19,14 @@ impl From<Quaternion> for YawPitchRoll {
             (2.0 * q.w * q.w + 2.0 * q.x * q.x - 1.0) as f64,
         );
         // pitch: (nose up/down, about Y axis)
-        let mut pitch = libm::atan2(
+        let pitch = libm::atan2(
             gravity.x as f64,
             libm::sqrt((gravity.y * gravity.y + gravity.z * gravity.z) as f64),
         );
         // roll: (tilt left/right, about X axis)
         let roll = libm::atan2(gravity.y as f64, gravity.z as f64);
 
-        pitch = PI - pitch;
+        //pitch = PI - pitch;
 
         Self {
             yaw: yaw as f32,
